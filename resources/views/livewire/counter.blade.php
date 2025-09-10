0b53fe381b822dec9662d382670d26d6d06a6dde
gabrieladrianofx <gabrieladrianofx@gmail.com>
2025-09-09 22:51:20 -0300

    acessando propriedades via js"

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -7,6 +7,13 @@
     <button wire:click="refresh">Refresh</button>
     <br><br>
 
+    <div x-data="">
+        <span x-text="$wire.name"></span>
+        <button @click="$wire.name = 'jeremias'">test</button>
+    </div>
+
+    <br><br>
+
     Name:: {{ $name }} {{ $lastName }}
     <div>
 
